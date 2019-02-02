075292938b444b5cafe7750df2c7963412026825
Anselm Brehme <development.anselm@breh.me>
2019-02-02 18:57:38 +0100

    impl could work like this

--- src/main/java/org/spongepowered/api/item/inventory/InventoryTransformation.java
+++ src/main/java/org/spongepowered/api/item/inventory/InventoryTransformation.java
@@ -42,13 +42,6 @@ import org.spongepowered.api.util.ResettableBuilder;
  */
 public interface InventoryTransformation {
 
-    interface Viewable extends InventoryTransformation {
-        InventoryArchetype type();
-
-        @Override
-        ViewableInventory transform(Inventory inventory);
-    }
-
     /**
      * Transforms an Inventory
      *
@@ -95,8 +88,6 @@ public interface InventoryTransformation {
          * @return The built transformation
          */
         InventoryTransformation build();
+
     }
-
-
-
 }
